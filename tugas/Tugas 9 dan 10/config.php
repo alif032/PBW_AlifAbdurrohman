c6a97facf50eb90093469cbf4ec15e3a979a5872
Alif Abdurrohman <148760951+alif032@users.noreply.github.com>
2025-06-14 16:21:31 +0700

    Add files via upload

---
+++ tugas/Tugas 9 dan 10/config.php
@@ -0,0 +1,3 @@
+<?php
+$base_url = "http://localhost/pbw/Tugas%209%20dan%2010/";
+?>
---
+++ tugas/Tugas 9 dan 10/includes/koneksi_db.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "pemograman_web";
+
+$conn = new mysqli($host, $user, $password, $database);
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
